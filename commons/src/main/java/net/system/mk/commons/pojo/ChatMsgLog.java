8c3783a293f4595b9905b16dec2a8e1e892e2a4b
codej9015@gmail.com <codej9015@gmail.com>
2026-01-25 00:38:13 +0800

    1.0F 前台架子搭建

--- commons/src/main/java/net/system/mk/commons/pojo/ChatMsgLog.java
+++ commons/src/main/java/net/system/mk/commons/pojo/ChatMsgLog.java
@@ -1,20 +1,44 @@
 package net.system.mk.commons.pojo;
 
+import com.alibaba.fastjson.annotation.JSONField;
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import java.time.LocalDateTime;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+import net.system.mk.commons.conf.MyLocalDateTimeDeserializer;
+import net.system.mk.commons.conf.MyLocalDateTimeSerializer;
+
+import java.time.LocalDateTime;
 
 /**
  * 聊天日志
  */
 @ApiModel(description = "聊天日志")
 @Data
-@EqualsAndHashCode(callSuper = true)
 @Accessors(chain = true)
-public class ChatMsgLog extends BasePO {
+public class ChatMsgLog{
+
+    @TableId(type = IdType.AUTO,value = "id")
+    private Integer id;
+
+    @TableField(fill = FieldFill.INSERT ,value = "create_at")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JSONField(serializeUsing = MyLocalDateTimeSerializer.class,deserializeUsing = MyLocalDateTimeDeserializer.class)
+    @JsonSerialize(using = MyLocalDateTimeSerializer.class)
+    @JsonDeserialize(using = MyLocalDateTimeDeserializer.class)
+    @ApiModelProperty(value = "创建时间")
+    private LocalDateTime createAt;
+
+    @ApiModelProperty(value = "启用状态：0-禁用，1-启用")
+    private Boolean disabled;
+
     /**
      * 聊天ID
      */
