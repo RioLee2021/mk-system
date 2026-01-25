d741ac50d75dfa854b8295a43af8240db5d44498
codej9015@gmail.com <codej9015@gmail.com>
2026-01-26 06:37:38 +0800

    添加公共上传接口

--- commons/src/main/java/net/system/mk/commons/dao/OrderRecordMapper.java
+++ commons/src/main/java/net/system/mk/commons/dao/OrderRecordMapper.java
@@ -23,6 +23,6 @@ public interface OrderRecordMapper extends BaseMapper<OrderRecord> {
             " from order_record ord inner join product_info pi on ord.product_id = pi.id left join order_pd_record opr on opr.order_no = ord.order_no left join order_pd_record opr2 on opr.order_no = ord.order_no and opr2.pd_status > 0 ${ew.customSqlSegment}")
     IPage<OrderRecordResponse> getPageByEw(IPage<OrderRecord> page, @Param("ew")QueryWrapper<Object> ew);
 
-    @Select("select ord.owner_id,mi.nickname,mi.phone,pi.create_at as register_at,ord.product_id,pi.pic_1_url,pi.product_name,pi.label_price,pi.order_price,pi.commission,pi.create_at from order_record ord inner join product_info pi on ord.product_id = pi.id left join mbr_info mi on ord.owner_id = mi.id where ord.order_no = #{orderNo}")
+    @Select("select ord.owner_id,mi.nickname,mi.phone,pi.create_at as register_at,ord.product_id,pi.pic1_url,pi.product_name,pi.label_price,pi.order_price,pi.commission,pi.create_at from order_record ord inner join product_info pi on ord.product_id = pi.id left join mbr_info mi on ord.owner_id = mi.id where ord.order_no = #{orderNo}")
     OrderRecordDetailResponse getDetailByOrderNo(@Param("orderNo")String orderNo);
 }
